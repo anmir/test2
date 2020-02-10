19c2ae291a1662d0155b5f693de8686ce84025c3
anmir <andrei_mir@mail.ru>
2020-02-10 22:20:17 +0300

    add files

---
+++ src/test/java/com/github/anmir/test1/Test1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anmir.test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
